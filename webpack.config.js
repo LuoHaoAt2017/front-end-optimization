63921481aa7f9f99e3ba5505596852c7f0a0c2a1
LuoHao-RenRen <hluo5206@gmail.com>
2020-04-19 21:09:33 +0800

    feat: 在react中使用svg图片的方式

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const CompressionWebpackPlugin = require('compression-webpack-plugin');
+//const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 function resolve(pathname) {
 	return path.resolve(__dirname, pathname);
@@ -33,24 +33,30 @@ module.exports = {
 				use: ['style-loader', 'css-loader', 'sass-loader']
 			},
 			{
-				test: /\.(png|jpg|gif|svg)$/,
-				use: ['file-loader']
+				test: /\.(jpeg|png|jpg|gif)$/,
+				loader: ['file-loader']
+			},
+			{
+				test: /\.svg$/,
+				exclude: /node_modules/,
+				loader: 'svg-react-loader',
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)/,
 				use: ['file-loader']
 			},
-			// {
-			// 	test: /\.(png|jpg|gif|svg)$/,
-			// 	use: [
-			// 		{
-			// 			loader: 'url-loader',
-			// 			options: {
-			// 				limit: 8192
-			// 			}
-			// 		}
-			// 	]
-			// }
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					{
+						//url-loader能将小于某个大小的图片进行base64格式的转化处理。
+						loader: 'url-loader',
+						options: {
+							limit: 2048
+						}
+					}
+				]
+			}
 		]
 	},
 	resolve: {
@@ -63,7 +69,15 @@ module.exports = {
 			template: resolve('./public/index.html')
 		}),
 		new CleanWebpackPlugin(),
-		// new WebpackBundleAnalyzer(),
-		// new CompressionWebpackPlugin()
-	]
+		//new WebpackBundleAnalyzer(),
+		new CompressionWebpackPlugin()
+	],
+	devServer: {
+		contentBase: resolve('assets'),
+		compress: true, // 一切服务都启用 gzip 压缩
+		port: 9000,
+		hot: true, // 启用模块热替换功能
+		open: 'Google Chrome',
+		stats: 'errors-only'
+	}
 };
