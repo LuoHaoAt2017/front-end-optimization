195673f1f2c52da35d5305004f9af6ca13b027bd
LuoHao-RenRen <hluo5206@gmail.com>
2020-04-24 09:08:32 +0800

    refactor: 添加Fragment目录

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,11 +3,6 @@ import './index.css';
 
 class Home extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.createPlaces = this.createPlaces.bind(this);
-    }
-
     render() {
         return (
             <div className="home">
@@ -15,23 +10,6 @@ class Home extends React.Component {
                 <div id="places"></div>
             </div>
         )
-    }
-
-    createPlaces() {
-        // DOM慢的原因：DOM回流导致页面元素几何尺寸和位置信息重新计算，
-        // DOM重绘导致页面元素非几何尺寸和位置信息的改变(背景色，字体颜色)。
-        // 减少DOM访问：将3次DOM操作，将少到一次。
-        const container = document.createDocumentFragment();
-        const places = ['Los Angeles', 'Houston', 'Detroit'];
-        const len = places.length;
-        for(let i = 0; i < len; i++) {
-            const span = document.createElement('span');
-            span.innerText = places[i];
-            span.setAttribute('class', 'place');
-            container.appendChild(span);
-        }
-        const placesDom = document.getElementById('places');
-        placesDom && placesDom.appendChild(container);
     }
     
     componentDidMount() {
@@ -42,7 +20,6 @@ class Home extends React.Component {
                 zoom: 8
             });
         }
-        this.createPlaces();
     }
 }
 
