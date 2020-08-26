8e7d0ef8e8b11ce088bff772ec596b3de2483957
luohao <luohao@authine.com>
2020-08-26 16:10:31 +0800

    perf: 图片懒加载优化，添加截流处理，已经加载的图片不必再次加载

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // import Home from '@/components/Home/index.js';
 // import About from '@/components/About/index.js';
-// import ImageLazyLoad from '@/components/LazyLoad';
+import ImageLazyLoad from '@/components/LazyLoad';
 // import Throttle from '@/components/Throttle';
-import Debounce from '@/components/Debounce';
+// import Debounce from '@/components/Debounce';
 
 function App() {
 	return (
 		<div className="app">
-			<Debounce/>
+			<ImageLazyLoad/>
 		</div>
 	);
 }
--- src/components/LazyLoad/index.js
+++ src/components/LazyLoad/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.scss';
+import throttle from 'lodash/throttle';
 
 class ImageLazyLoad extends React.Component {
     constructor() {
@@ -48,22 +49,41 @@ class ImageLazyLoad extends React.Component {
     }
 
     handleScroll() {
+        console.log('scroll');
         const viewH = document.documentElement.clientHeight;
         const images = document.getElementsByClassName('img');
+        let counter = 0;
         for (let i = 0; i < images.length; i++) {
             const target = images[i];
-            const top = target.getBoundingClientRect().top;
-            if (top < viewH) {
-                target.src = target.dataset.src;
+            if (!target.src && target.dataset) {
+                const top = target.getBoundingClientRect().top;
+                const delte = 50; // 50px的缓冲距离
+                const loaded = this.state.loaded;
+                if (top < viewH + delte) {
+                    target.src = target.dataset.src;
+                    console.log('为什么更改loaded无效');
+                    // 为什么更改loaded无效
+                    counter++;
+                }
             }
+        }
+        // setState异步操作
+        this.setState({
+            loaded: this.state.loaded + counter
+        });
+    }
+
+    componentDidUpdate(props, state) {
+        // 已经加载完毕全部图片后，注销scroll的回调函数。
+        if (state.loaded === state.images.length) {
+            window.removeEventListener('scroll', this.handleScroll);
         }
     }
 
     componentDidMount() {
+        this.handleScroll = throttle(this.handleScroll, 500);
         this.handleScroll();
-        window.addEventListener('scroll', () => {
-            this.handleScroll();
-        });
+        window.addEventListener('scroll', this.handleScroll);
     }
 }
 
